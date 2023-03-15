3ff65523f9c8b5cbb054446bbecc6bbebec112bb
sebas7389 <sebastian7389@gmail.com>
2023-03-14 21:35:25 -0300

    express funcionando y ruta raiz get definida

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const app = express ();
+const port =4000; 
+
+app.get('/' , (request, response) => {
+    response.send ({
+        msg: `Bienvenido a mi servidor express`,
+        ok:true
+    })
+})
+app.listen(port , () => {
+    console.log(`Servidor funcionando en puerto ${port}`);
+})
