1f47c1d89925c5ed0570d300635419bb9f1ecc6e
sebas739 <sebastian7389@gmail.com>
2023-04-11 10:11:48 -0300

    mongoose, rutas, controladores y esquemas de producto en progeso

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const productRoutes = require('./routes/product.routes')
-
+const productRoutes = require("./routes/product.routes");
 
 //middlewares
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
-
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (request, response) => {
-    response.send({
-        msg: `Bienvenidos a mi servidor express`,
-        ok: true
-    })
+  response.send({
+    msg: `Bienvenidos a mi servidor express`,
+    ok: true,
+  });
 });
 
 // Definir rutas a usar por mi app express.
-app.use(productRoutes)
-
-
-
+app.use(productRoutes);
 
 module.exports = app;
--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,92 +1,94 @@
-const Product = require('./../schemas/product.schema')
+const Product = require("./../schemas/product.schema");
 
 const getAllProducts = (req, res) => {
-    Product.find().then(function(productos){
-
-        res.status(200).send({
-            msg: `Productos obtenidos correctamente`,
-            productos: productos
-        });
-    }).catch((error)=> {
-        console.log(error)
+  Product.find()
+    .then(function (productos) {
+      res.status(200).send({
+        msg: `Productos obtenidos correctamente`,
+        productos: productos,
+      });
     })
-}
-
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
 function addProduct(req, res) {
-    console.log(req.body)
-    const product = new Product(req.body);
-    console.log(product)
+  console.log(req.body);
+  const product = new Product(req.body);
+  console.log(product);
 
-    product.save()
-                .then(function(product) {
-                    return res.status(200).send({
-                        msg: `Producto guardado correctamente`,
-                        product
-                    })
-
-                })
-                .catch(error => {
-                    console.log(error);
-                    res.status(500).send(`El producto no se pudo guardar`)
-                })
+  product
+    .save()
+    .then(function (product) {
+      return res.status(200).send({
+        msg: `Producto guardado correctamente`,
+        product,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).send(`El producto no se pudo guardar`);
+    });
 }
 
-
 function deleteProduct(req, res) {
-    const id = req.params.id; //user@pepito.com
+  const id = req.params.id; //user@pepito.com
 
-    Product.findByIdAndDelete(id)
-        .then((deleted) => {
-            if(!deleted) {
-                return res.status(404).send({
-                    msg: `No se encontro el producto a borrar`
-                })    
-            }
-            return res.status(200).send({
-                msg: `Producto borrado correctamente`,
-                deleted
-            })
-        })
-        .catch(error => {
-            console.log(error);
-            return res.status(500).send({
-                msg: `Error al borrar el producto`
-            })
-        })
+  Product.findByIdAndDelete(id)
+    .then((deleted) => {
+      if (!deleted) {
+        return res.status(404).send({
+          msg: `No se encontro el producto a borrar`,
+        });
+      }
+      return res.status(200).send({
+        msg: `Producto borrado correctamente`,
+        deleted,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).send({
+        msg: `Error al borrar el producto`,
+      });
+    });
 }
 
 function getProduct(req, res) {
-    const id =req.query.id;
+  const id = req.query.id;
 
-if(!id) {
+  if (!id) {
     return res.status(400).send({
-        msg:`Es necesario que mande un ID`
-    })
-    
-}    
-product.findById(id).then((product)=> {
-    //2 Casos posibles en una peticion correcta:
-    //a- El id proporcionado no corresponde a ningun producto
-    if(!product) {
+      msg: `Es necesario que mande un ID`,
+    });
+  }
+  product
+    .findById(id)
+    .then((product) => {
+      //2 Casos posibles en una peticion correcta:
+      //a- El id proporcionado no corresponde a ningun producto
+      if (!product) {
         return res.status(400).send({
-            msg:`No se encontro el producto`
-        })
-    }
-    // b- Se encontro el producto
-    return res.status(200).send({
-        msg:`Se encontro el producto`
+          msg: `No se encontro el producto`,
+        });
+      }
+      // b- Se encontro el producto
+      return res.status(200).send({
+        msg: `Se encontro el producto`,
+      });
     })
-}) .catch(error => {
-           console.log(error);
-           return res.status(500).send({
-                msg:`Error al obtener producto`
-            })
-         })
-J  }
+    .catch((error) => {
+      console.log(error);
+      return res.status(500).send({
+        msg: `Error al obtener producto`,
+      });
+    });
+  J;
+}
 
 module.exports = {
-    getAllProducts,
-    deleteProduct,
-    addProduct
-}
+  getAllProducts,
+  deleteProduct,
+  addProduct,
+};
