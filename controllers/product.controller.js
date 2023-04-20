69e106d6c4a96fd1b4ed9c0f72afd89eafba2f42
sebas739 <sebastian7389@gmail.com>
2023-04-20 19:39:36 -0300

    cambiando repo

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,12 +1,14 @@
 const Product = require("./../schemas/product.schema");
 
 const getAllProducts = (req, res) => {
-  Product.find()
-    .then(function (productos) {
+    
+  Product.find().then(function (productos) {
+
       res.status(200).send({
         msg: `Productos obtenidos correctamente`,
         productos: productos,
       });
+
     })
     .catch((error) => {
       console.log(error);
